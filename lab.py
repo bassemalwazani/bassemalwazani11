4a99339e9341b979ef4274705dc56d0d95242653
bassemalwazani <154684592+bassemalwazani@users.noreply.github.com>
2023-12-26 14:02:37 +0300

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,38 @@
+#1
+# list=[10,4,2]
+# i=1
+# for x in list:
+#         i*=x
+# print(i)
+#2
+# list=[10,15,12,13,20,21]
+# l=[]
+# for x in list:
+#     if(x%2!=0):
+#         l.append(x)
+# print(l)
+#3
+# str='khokho'
+# mid=len(str)//2
+# num1=str[0:mid]
+# num2=str[mid:]
+# # print(num1,num2)
+# if num1==num2:
+#     print("symmetrical")
+#4
+# def sym(s1):
+#     str='khokho'
+#     mid=len(str)//2
+#     num1=str[0:mid]
+#     num2=str[mid:]
+#     if num1==num2:
+#      print("symmetrical")
+# sym("menmom")
+# sym("movemove")
+#5
+# x="toot"
+# str2=''
+# for s2 in range(len(x)-1,-1,-1):
+#     str2+=x[s2]
+# if x==str2:
+#     print('polyndrome')
